38c3e9eb806a3d7f3d00ea16fc721903fcec0010
Arya K <aryapreethi045@gmail.com>
2026-04-07 12:21:15 +0530

    Added CI/CD pipeline

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,16 +1,29 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+
 class ApiService {
-  static const baseUrl = "https://fakestoreapi.com/products";
+  static const String baseUrl =
+      "https://fakestoreapi.com/productss";
 
   Future<List<dynamic>> getProducts() async {
-    final response = await http.get(Uri.parse(baseUrl));
+    try {
+      final response = await http.get(Uri.parse(baseUrl));
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception("Failed to load products");
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception(
+          "Server error: ${response.statusCode}",
+        );
+      }
+    } on http.ClientException {
+      throw Exception("No internet connection");
+    } on FormatException {
+      throw Exception("Invalid data format");
+    } catch (e) {
+      throw Exception("Something went wrong: $e");
     }
   }
 }
+
